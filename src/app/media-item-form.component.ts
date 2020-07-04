c2ee74c37af1b91515c1d2dac1c065959968bf9a
ypearl84 <y.pearl84@gmail.com>
2020-07-04 15:35:46 -0700

    commit 2020.07.04.05

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -12,10 +12,30 @@ export class MediaItemFormComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      name: new FormControl('',
+        Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
+
+  // custome validator
+  yearValidator(control: FormControl) {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: true };
+    }
   }
 
   onSubmit(mediaItem) {
