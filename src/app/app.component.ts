10c157c0620098d1a13b1559575a4dc91cb991d4
ypearl84 <y.pearl84@gmail.com>
2020-07-03 20:18:57 -0700

    commit 2020.07.03.04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  //when you want to put the template to direct to component -> use 'template'
+  templateUrl:'./app.component.html',
+  //when you want to put css to direct to component -> use 'styles'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
