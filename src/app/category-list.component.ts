ca09e78903ca15e1c2c482e912933cb5b0b81855
ypearl84 <y.pearl84@gmail.com>
2020-07-05 16:15:38 -0700

    commit 2020.07.05.04

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -9,13 +9,15 @@ import { Component, Input } from '@angular/core';
   `,
   styles: [
     `
-      .label {
-        background-color: #999999;
-        color: #ffffff;
-        border-radius: 4px;
-        padding: 2px 6px;
-        font-style: italic;
-        width: max-content;
+      :host {
+        display : block;
+        margin-bottom: 20px;
+      }
+      :host-context(.medium-movies) span {
+        background-color: #53ace4;
+      }
+      :host-context(.medium-series) span {
+        background-color: #45bf94;
       }
       span {
         display: inline-block;
